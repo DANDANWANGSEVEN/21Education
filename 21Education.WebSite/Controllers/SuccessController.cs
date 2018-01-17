9c474160d613342cf63e152f98de14aed2038eb1
Administrator <Administrator@USER-T06TCCE3OJ>
2018-01-17 10:24:58 +0800

    design image

--- 21Education.WebSite/Controllers/SuccessController.cs
+++ 21Education.WebSite/Controllers/SuccessController.cs
@@ -15,6 +15,9 @@ namespace _21Education.WebSite.Controllers
         {
             return View();
         }
-
+        public ActionResult SuccessList()
+        {
+            return View();
+        }
     }
 }

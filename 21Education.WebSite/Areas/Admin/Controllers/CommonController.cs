37c4654e3971c43526f85d0bb89cfb86e2d80488
630383257 <630383257@qq.com>
2018-03-15 01:47:45 +0800

    change style

---
+++ 21Education.WebSite/Areas/Admin/Controllers/CommonController.cs
@@ -0,0 +1,18 @@
+﻿using _21Education.COMMON;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web;
+using System.Web.Mvc;
+
+namespace _21Education.WebSite.Areas.Admin.Controllers
+{
+    public class CommonController : Controller
+    {
+        public void Upload()
+        {
+
+        }
+    }
+}
